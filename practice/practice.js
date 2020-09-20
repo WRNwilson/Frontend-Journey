6159f44892a4c040b5ae72de121a3c3779e6df78
Samael189 <41208480+Samael189@users.noreply.github.com>
2020-09-20 12:48:40 -0400

    Add files via upload

---
+++ practice/practice.js
@@ -0,0 +1,12 @@
+function changeCase(str) {
+  let result = '';
+  for (let i=0; i<str.length; i++) {
+	  if(i % 2 == 0){
+	  	result += str[i].toLowerCase();
+	  	}else{
+	  		result += str[i].toUpperCase();
+	  		}
+	  		     }
+  return result;
+}
+ console.log(changeCase('Panda Squad'));	
